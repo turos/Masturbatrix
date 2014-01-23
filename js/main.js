bde6647eb52bd89f7b5b5a1470481009c539df0e
Tue Rossel <tuerossel@gmail.com>
2014-01-23 10:34:04 +0100

    Adding internal anchor scroll with JQuery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 	
-	//transfering from main page to data page with selected topic
-	$("#selectTopic").change(function(){
-		var url = "data.html?topic=" + $("#selectTopic").val();
-		window.open(url,'_self',false);
-	});
+	$('a[anchor]').click(function(e) {
+        e.preventDefault();
+        var anchor = $(this).attr('anchor');
+        var top = $('div[name='+anchor+']').offset().top;
+        $('html, body').animate({scrollTop:top},2000);
+    });
+
 
 
 });
